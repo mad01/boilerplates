5f191cd0be955eb6dae1a272715233ee682c34c9
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2015-08-15 15:36:17 +0200

    adding chrome install script and selenium.plist osx service file

---
+++ osx/chrome-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+curl -L -O "https://dl.google.com/chrome/mac/stable/GGRO/googlechrome.dmg"
+hdiutil mount -nobrowse googlechrome.dmg
+cp -R "/Volumes/Google Chrome/Google Chrome.app" /Applications
+hdiutil unmount "/Volumes/Google Chrome"
+rm googlechrome.dmg
