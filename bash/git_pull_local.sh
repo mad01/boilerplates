c9db0e43af1caa1dc873951561e7f8146a5f984e
Alexander Brandstedt <alexanderbrandstedt@gmail.com>
2015-11-08 17:17:29 +0100

    update to use printf not echo

--- bash/git_pull_local.sh
+++ bash/git_pull_local.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-echo "Pull git repos in currect directory"
-
 GREEN='\033[0;32m'
 LIGHTGREEN='\033[1;32m'
 NC='\033[0m'
 folderArray=()
 DIR=$(pwd)
 
+printf "${GREEN}Pull git repos in currect directory ${NC}"
+
 for FILE in $DIR/*; do
     [[ -d $FILE ]] && folderArray+=("$FILE")
     echo $FILE
 done
 
-for item in "${folderArray[@]}"
+for folder in "${folderArray[@]}"
 do
-    echo " "
-    printf  "${GREEN}pull latest of git repo: ${LIGHTGREEN}$item ${NC}"
-    pushd $item
+    printf "\n"
+    printf "${GREEN}pull latest of git repo: ${LIGHTGREEN} $folder${NC}"
+    pushd $folder
     git pull -p
     popd
-    echo " "
+    printf "\n"
 done
